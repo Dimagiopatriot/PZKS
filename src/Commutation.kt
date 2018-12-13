264cb412c735a48cddfccb0d6cbd5dc9d53f080b
Dimagiopatriot <troll3758@gmail.com>
2018-12-13 08:31:05 +0200

    update

--- src/Commutation.kt
+++ src/Commutation.kt
@@ -7,12 +7,25 @@ class Commutation(private val enterExpression: Expression.Binary) {
 
     fun commutateExp() {
         decomposeExpressionToNodes(enterExpression)
-        val sortedNodes = nodesList.sortedBy { it.priority }
-        sortedNodes.forEach {
-            newExpressionInStringFormat += if (it == sortedNodes.last()){
-                "$it "
-            } else {
-                "$it ${LexerAnalyzer.Symbol[it.parent!!.opr]} "
+        val copy = mutableSetOf<Expression>()
+        copy.addAll(nodesList)
+        copy.forEach { if (it is Expression.Binary) removeChilds(it) }
+        val sortedNodes = nodesList.sortedBy { it.priority }.filter {
+            it.parent != null && (it == it.parent?.right || it == it.parent?.left)
+        }
+        newExpressionInStringFormat += "${sortedNodes[0]}"
+        sortedNodes.dropLast(1).forEachIndexed { index, _ ->
+            val nextIt = sortedNodes[index + 1]
+            nextIt.parent?.let { parent ->
+                val parentOfParent = parent.parent
+                if (parentOfParent != null && parentOfParent.right == parent) {
+                    val parentOfParentOpr = parentOfParent.opr
+                    when (listOf(parentOfParentOpr, parent.opr)) {
+                        listOf(5, 5), listOf(5, 4) -> newExpressionInStringFormat += " ${LexerAnalyzer.Symbol[5]} $nextIt"
+                    }
+                } else {
+                    newExpressionInStringFormat += " ${LexerAnalyzer.Symbol[parent.opr]} $nextIt"
+                }
             }
         }
     }
@@ -31,18 +44,36 @@ class Commutation(private val enterExpression: Expression.Binary) {
                         nodesList.add(expression)
                     }
                     if (parent.right !is Expression.Binary && parent.left !is Expression.Binary) {
-                        val parentOfParent = parent.parent
-                        if (parentOfParent != null) {
-                            if (parentOfParent.opr == 4) {
-                                parent.priority = 1
-                                nodesList.add(parent)
-                            } else {
-                                parentOfParent.priority = 2
-                                nodesList.add(parentOfParent)
-                            }
+                        var parentOfParent = parent.parent
+                        while (true) {
+                            if (parentOfParent != null) {
+                                if (parentOfParent.opr == 4) {
+                                    parent.priority = 1
+                                    nodesList.add(parent)
+                                    break
+                                } else {
+                                    parentOfParent.priority = 2
+                                    nodesList.add(parentOfParent)
+                                    parentOfParent = parentOfParent.parent
+                                }
+                            } else
+                                break
                         }
                     }
                 }
+            }
+        }
+    }
+
+    fun removeChilds(parentExp: Expression.Binary) {
+        var stairsExp = parentExp
+        while (stairsExp.right is Expression.Binary || stairsExp.left is Expression.Binary) {
+            nodesList.removeIf { it == stairsExp.right || it == stairsExp.left }
+            val goLeft = stairsExp.left
+            if (goLeft is Expression.Binary) {
+                stairsExp = goLeft
+            } else {
+                break
             }
         }
     }
